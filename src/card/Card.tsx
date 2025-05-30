0a24158a0f3692cffb2369fd29b2c9dc5a4316b3
Ismail40376 <dlatiktoka304@gmail.com>
2025-05-30 12:50:10 +0500

    added a favorite button

--- src/card/Card.tsx
+++ src/card/Card.tsx
@@ -1,15 +1,26 @@
-import Raiting from "../rating/Rating";
+import type { FC } from "react";
+import heartfilled from "../../public/Heart_filled.svg";
+import heartEmpty from "../../public/heart-icon.png";
+import Rating from "../rating/Rating";
 import s from "./Card.module.scss";
 
-const Card = () => {
+interface CardProps {
+  rating: number;
+  title: string;
+  price: number;
+  isFavorite: boolean;
+}
+
+const Card: FC<CardProps> = ({ rating, title, price, isFavorite }) => {
   return (
     <div className={s.cardWrap}>
-      <Raiting rating={3} />
-      <p className={s.stars}></p>
+      <Rating rating={rating} />
       <img src="/clothes.png" alt="clothes" />
-      <p className={s.heart}>heart</p>
-      <p>title</p>
-      <p>price</p>
+      <button className={s.heart}>
+        <img src={isFavorite ? heartfilled : heartEmpty} alt="heart" />
+      </button>
+      <p>{title}</p>
+      <p>{price}</p>
     </div>
   );
 };
